fe80de32f14de75198b22c2b53c458d248baf6fa
leoarthurwall <leowall93@gmail.com>
2022-09-19 12:50:53 +0100

    added form, input and label to custom form. also installed heroicons and imported an icon for the button on the custom form

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,9 +5,9 @@ import { CustomForm } from "./components/CustomForm";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <header>
-        <h1>hello world</h1>
+        <h1>My Task List</h1>
       </header>
       <CustomForm />
     </div>
--- todo-list/src/components/CustomForm.js
+++ todo-list/src/components/CustomForm.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from "react";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 const handleFormSubmit = (e) => {
-    e.preventDefailt();
-    console.log(e);
-}
+  e.preventDefailt();
+  console.log(e);
+};
 
 export const CustomForm = () => {
   return (
-    <form
-    className='todo'
-    onSubmit={handleFormSubmit}
-    >
-    <div className='wrapper'>
-        <h1>hello</h1>
-    </div>
+    <form className="todo" onSubmit={handleFormSubmit}>
+      <div className="wrapper">
+        <input
+          type="text"
+          id="task"
+          className="input"
+          // value={task}
+          // inInput={(e) => setTask(e.target.value)}
+          required
+          autoFocus
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          Enter Task
+        </label>
+      </div>
+      <button className="button" aria-label="Add Task" type="submit">
+        <BeakerIcon className="h-6 w-6 text-blue-500" />
+      </button>
     </form>
-    )
-}
+  );
+};
