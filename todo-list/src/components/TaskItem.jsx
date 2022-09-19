e41352479cfbdd0bb6bece9c3f328a1896ab62bf
leoarthurwall <leowall93@gmail.com>
2022-09-19 19:20:54 +0100

    added code to run a line through the checkbox text once the checkbox is marked complete. added state to check the status of the checkbox: true, false and then make a line through if true

--- todo-list/src/components/TaskItem.jsx
+++ todo-list/src/components/TaskItem.jsx
@@ -1,11 +1,22 @@
+//library imports
+import { CheckIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
 export default function TaskItem({ task }) {
+    const [isChecked, setIsChecked] = useState(task.checked)
+
+    const handleCheckboxChange = (e) => {
+        setIsChecked(!isChecked)
+    }
+
   return (
     <li className="task-item">
       <div className="task-group">
         <input
           type="checkbox"
-          checked="task.checked"
-          // onChange={}
+          className="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
           name={task.name}
           id={task.id}
         />
